fc8f1212bb85bcd5c9ab00784ff6dc712824668c
Biagio Trimarchi <95865775+biagio-trimarchi@users.noreply.github.com>
2023-08-26 20:53:43 +0200

    Changes: + added .aliases, file for aliases in the .bashrc + added .gitconfig, to set automatically set up git * modified ubuntu_install.sh to automate the installation of the latters

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -8,5 +8,11 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # BASHRC
 ln -s "$DOTFILES_DIR"/.bashrc "$HOME"/.bashrc  
 
+# ALIASES (my aliases for the bashrc)
+ln -s "$DOTFILES_DIR"/.aliases "$HOME"/.aliases  
+
 # VIMRC
 ln -s "$DOTFILES_DIR"/.vimrc "$HOME"/.vimrc  
+
+# GIT
+ln -s "$DOTFILES_DIR"/.gitconfig "$HOME"/.gitconfig 
